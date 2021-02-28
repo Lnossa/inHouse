fc8716832ee16c75ba7a8f1752a97c2669b8ee67
liviunossa <liviunossa@gmail.com>
2021-02-28 13:30:11 +0200

    Fix build

--- build_ms.bat
+++ build_ms.bat
@@ -1,9 +1,10 @@
 @echo off
 
+set CMAKE_BUILD_PARALLEL_LEVEL=%NUMBER_OF_PROCESSORS%
 set CC=C:/SysGCC/raspberry/bin/arm-linux-gnueabihf-gcc.exe
 set CXX=C:/SysGCC/raspberry/bin/arm-linux-gnueabihf-g++.exe
 
 cd Build
 cmake ../ -G "Unix Makefiles" 
-make
+cmake --build . -- -Oline
 cd ..
